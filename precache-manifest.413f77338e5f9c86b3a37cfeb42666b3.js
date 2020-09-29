06a542c2078dce9d0155b9bb7f5219ad1e7dd3e6
rawah <rawah@pdx.edu>
2020-09-29 08:44:01 -0700

    Updates

---
+++ precache-manifest.413f77338e5f9c86b3a37cfeb42666b3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce77006b71587bcdd1d1d6779b5df0dd",
+    "url": "/simple-portfolio/index.html"
+  },
+  {
+    "revision": "7e6299cc13e3782951ec",
+    "url": "/simple-portfolio/static/css/main.c4b02a71.chunk.css"
+  },
+  {
+    "revision": "f5acf4831ed023a7475c",
+    "url": "/simple-portfolio/static/js/2.c8b18b02.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-portfolio/static/js/2.c8b18b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6299cc13e3782951ec",
+    "url": "/simple-portfolio/static/js/main.e5098f71.chunk.js"
+  },
+  {
+    "revision": "1dc9fd171c901666c04c",
+    "url": "/simple-portfolio/static/js/runtime-main.3cc36f48.js"
+  },
+  {
+    "revision": "d347ab42ff513d211d59e6126a643425",
+    "url": "/simple-portfolio/static/media/agile.d347ab42.png"
+  },
+  {
+    "revision": "5c5704b5536dae79644dcfb41f55c245",
+    "url": "/simple-portfolio/static/media/art.5c5704b5.jpg"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/simple-portfolio/static/media/css.75e4d6d5.png"
+  },
+  {
+    "revision": "bde6d1ec373bfa698c94fc6168e57e1c",
+    "url": "/simple-portfolio/static/media/git.bde6d1ec.png"
+  },
+  {
+    "revision": "9f31da4bab9f678b1227519a7686c930",
+    "url": "/simple-portfolio/static/media/github.9f31da4b.svg"
+  },
+  {
+    "revision": "31039a53ab57f00b4eff33fb7bcfa2e8",
+    "url": "/simple-portfolio/static/media/html.31039a53.png"
+  },
+  {
+    "revision": "3d40302b0864db8e2210e295152d8543",
+    "url": "/simple-portfolio/static/media/mongodb.3d40302b.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/simple-portfolio/static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "182e98238de8c18dd7974a6cb32a13c2",
+    "url": "/simple-portfolio/static/media/python.182e9823.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/simple-portfolio/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "e4d536b1a07294afad43045def94294f",
+    "url": "/simple-portfolio/static/media/sql.e4d536b1.png"
+  }
+]);
